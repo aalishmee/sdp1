7f2acfe9b077f890f866cc947137101e46c29a5a
aalishmee <251977@astanait.edu.kz>
2026-09-13 18:27:02 +0500

    Add files via upload

---
+++ MargheritaBuilder.java
@@ -0,0 +1,33 @@
+package pizza;
+
+public class MargheritaBuilder implements PizzaBuilder {
+    private Pizza pizza = new Pizza();
+
+    public void setSize(String size) {
+        pizza.setSize(size);
+    }
+
+    public void setCrust(String crust) {
+        pizza.setCrust(crust);
+    }
+
+    public void setSauce(String sauce) {
+        pizza.setSauce(sauce);
+    }
+
+    public void setCheese(String cheese) {
+        pizza.setCheese(cheese);
+    }
+
+    public void addToppings(String toppings) {
+        pizza.setToppings(toppings);
+    }
+
+    public void setExtraCheese(boolean extraCheese) {
+        pizza.setExtraCheese(extraCheese);
+    }
+
+    public Object getResult() {
+        return pizza;
+    }
+}
